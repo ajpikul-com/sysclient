d037fd707bdf32879fdbde9f717a02dcb93fbe25
Andrew Pikul <ajpikul@gmail.com>
2023-08-02 17:57:25 +0000

    Change logging level

--- log.go
+++ log.go
@@ -10,7 +10,7 @@ var defaultLogger ilog.LoggerInterface
 
 func initLogger() {
 	defaultLogger = &ilog.SimpleLogger{}
-	defaultLogger.(*ilog.SimpleLogger).Level(ilog.DEBUG)
+	defaultLogger.(*ilog.SimpleLogger).Level(ilog.INFO)
 	err := defaultLogger.Init()
 	if err != nil {
 		panic(err)
